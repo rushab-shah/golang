53dd687e66f12b23583dbf60d1125e80d6652bac
Rushab Shah <rushabshah@rushabs-air.lan>
2023-03-11 12:00:37 -0500

    Values code

---
+++ values/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Checking " + "Values!")
+	fmt.Println("1+1= ", 1+1)
+	fmt.Println("10.0/5.0= ", 10.0/5.0)
+	fmt.Println(true && true)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+}
